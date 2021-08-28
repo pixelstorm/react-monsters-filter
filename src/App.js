78d1618ccffe09966b524ab1edaad887f5955d20
pixelstorm <dan@pixelstorm.com.au>
2021-08-28 18:22:35 +1000

    pass simple name prop to CardList component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import { CardList } from "./components/card-list.component";
 import "./App.css";
 
 class App extends Component {
@@ -26,6 +26,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <CardList name="brian" />
         {this.state.monsters.map((monster) => (
           <h1 key={monster.id}>{monster.name}</h1>
         ))}
