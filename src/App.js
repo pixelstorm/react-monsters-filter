5506b69a96c28365b3a3f7d268b568e0f7bc3269
pixelstorm <dan@pixelstorm.com.au>
2021-08-28 18:56:33 +1000

    pass the state to cardlist as a prop

--- src/App.js
+++ src/App.js
@@ -26,10 +26,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <CardList name="brian" />
-        {this.state.monsters.map((monster) => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+        <CardList monsters={this.state.monsters} />
       </div>
     );
   }
--- src/components/card-list.component.jsx
+++ src/components/card-list.component.jsx
@@ -1,12 +1,11 @@
 import React from  'react'
-export const CardList = (props) => {
-
-    const {name} = props; 
-
-
-
+export const CardList = props => {
     return (
-      <div>{name}</div>
+      <div>
+          {props.monsters.map((monster) => (
+            <h1 key={monster.id}>{monster.name}</h1>
+          ))}
+        </div>
     ) 
   }
-  export default CardList
+ // export default CardList
